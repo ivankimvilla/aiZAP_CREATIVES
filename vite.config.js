9b405412e078a9a576da1bb1260b8ea68614660d
ivankimvilla <ivanalmadin0@gmail.com>
2026-07-29 14:46:39 +0800

    fix header

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,19 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css',
+                'resources/css/header.css',
                 'resources/css/home-page.css',
+                'resources/css/footer.css',
+                'resources/css/button.css',
+                'resources/css/about-us.css',
+                'resources/css/pages-shared.css',
+                'resources/css/portfolio.css',
+                'resources/css/pricing.css',
+                'resources/css/process.css',
+                'resources/css/services.css',
+                'resources/css/video-modal.css',
+                'resources/css/admin/index.css',
                 'resources/js/app.js',
                 'resources/js/pages/home.js',
                 'resources/js/pages/portfolio.js',
